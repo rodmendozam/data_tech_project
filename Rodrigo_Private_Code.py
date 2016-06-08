1b78c2d4425e2f4092e8b57af2c1e4359f00a07e
unknown <s150834@s150834.campus.tue.nl>
2016-06-08 21:19:54 +0200

    no message

--- Rodrigo_Private_Code.py
+++ Rodrigo_Private_Code.py
@@ -120,73 +120,110 @@ def f_value(adj, x, v, i, t_start, t_end):
             cost = float(nb[1])
     return min, cost
 
+def computerUFJ_latest(s, adjl, t_start, t_end):
+    # pq = [] #priority quee min-heap
+    d = {}
+    open = {}
+    open[s] = True
+    d[s] = True
+    father = {}
+    for key in adjl:
+        father[key] = None
+        open[key] = True
+        if (str(key) != s):
+            d[key] = math.inf
+        else:
+            d[key] = 0
+            # d2 = 0 #depth
 
+            # father2 = None # ?
+            # entry = [d2, father2, node]
+            # entry = [d2, key]
+            # heapq.heappush(pq, entry)
+            add_task(key, 0)
+    while pq:
+        rootNode = pop_task()
+        open[rootNode] = False
+
+        if rootNode not in adjl:
+            continue
+
+        for neighbour in adjl[rootNode]:
+            if neighbour[0] not in open:
+               open[neighbour[0]] = True
+            if neighbour[0] not in d:
+                d[neighbour[0]] = math.inf
+            if open[neighbour[0]]:
+                f_score, nb_element_cost = f_value(adjl, rootNode, neighbour[0], d[rootNode], t_start, t_end)
+
+                #RELAX
+                if f_score + nb_element_cost  < d[neighbour[0]]:
+                    d[neighbour[0]] = f_score + nb_element_cost
+                    father[neighbour[0]] = rootNode
+
+                #Update heap
+                add_task(neighbour[0], d[neighbour[0]])
+
+    return d, father
+
+def initDict(src, value):
+    f = open(src)
+    dict = {}
+    for line in iter(f):
+        u, v, alpha, t = line.split()
+        dict[u] = value
+        dict[v] = value
+    f.close()
+    return dict
+
+def earliest_arrival_time(dict, x, t_start, t_end, f):
+    # timeStart = time.time()
+    dict[x] = t_start
+    for line in iter(f):
+
+        u, v, alpha, t = line.split()
+        u, v, alpha, t = str(u), str(v), float(alpha), float(t)
+        if (t+alpha) <= t_end and t >= float(dict[u]):
+            if t+alpha < float(dict[v]):
+                dict[v] = t + alpha
+        elif t >= t_end:
+            break
+    # return time.time() - timeStart
+    return dict
 
 if __name__ == "__main__":
 
-    my_adj = makeAdjacencyList('dataset/ufj_test.csv')
 
-    dd, ff = computerUFJ('A', my_adj, 0, math.inf)
-    print(dd)
-    print('Father section')
-    print(ff)
+    #earliest test
+    earliest_src_file = 'dataset/test_earliest.csv'
+    f = open(earliest_src_file)
+    db = initDict(earliest_src_file, math.inf)
+    # print(db)
+    print(earliest_arrival_time(db, 'A', 0, math.inf, f))
+
+
+
+
+
+
+    #test numpy
+
+    # import numpy as np
+    # my_array = np.empty(841372, dtype=object)
+    # line = '1 2 1 567897'
+    # a, b, c, d = line.split()
+    # np.put(my_array, [0], a)
+    # print(my_array)
+
+
+    #test for latest on Xuan
+    # my_adj = makeAdjacencyList('dataset/ufj_test.csv')
+    # dd, ff = computerUFJ_latest('A', my_adj, 0, math.inf)
+    # print(dd)
+    # print('Father section')
+    # print(ff)
     # print(f_value(my_adj, 'A', 'B', 0, 0, math.inf))
 
-
-    # add_task( 'A', 3)
-    # add_task( 'B', 1)
-    # add_task( 'C', 2)
-    # add_task( 'D', 1)
-    # add_task( 'F', 0)
-    # print(pq)
-    #
-    # add_task( 'C', 1)
-    # print( pop_task() )
-    # # print( pop_task() )
-    # # print( pop_task() )
-    # # print( pop_task() )
-    #
-    # print(pq)
-
-
-    #
-    # ordered = []
-    # while pq:
-    #     value = heapq.heappop(pq)
-    #     ordered.append(pop_task())
-    # print(ordered)
-
-
-    #create adj lst
-    # print(makeAdjacencyList('dataset/test.csv'))
-
-    # my_adj = makeAdjacencyList('dataset/test.csv')
-    #
-    # for key in my_adj:
-    #     print(type(key))
-
-
-    # heap_test = []
-    # heapq.heappush(heap_test, (1, ''))
-    #
-    #
-    # # search for an element by key in a min heap
-    # heap = []
-    # heapq.heappush(heap, Element('B', 2))
-    # heapq.heappush(heap, Element('C', []))
-    # heapq.heappush(heap, Element('A', 1))
-    # heapq.heappush(heap, Element('A', 22))
-    #
-    # # heapq.heapreplace(Element('A', 1),Element('A', 222))
-    #
-    # print( search_in_heap_element(heap, Element('A', 1)) )
-    #
-    # #print heap example
-    # ordered = []
-    # while heap:
-    #     value = heapq.heappop(heap)
-    #     ordered.append([value.key, value.value])
-    # print(ordered)
 
 
 
